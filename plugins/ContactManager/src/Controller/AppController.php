b4c06e8f51d4f40d471cb6a9374859c4540be810
iman <iman@bitmascot.com>
2018-11-07 14:15:30 +0600

    plugin layout load

--- plugins/ContactManager/src/Controller/AppController.php
+++ plugins/ContactManager/src/Controller/AppController.php
@@ -3,8 +3,12 @@
 namespace ContactManager\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Event\Event;
 
 class AppController extends BaseController
 {
-
+    public function beforeFilter(Event $event)
+    {
+        $this->viewBuilder()->setLayout('admin');
+    }
 }
